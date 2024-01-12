650ee63b61121014426f77e7c5e4e49e22e0144a
Braldo <braldo.montiel@gmail.com>
2024-01-11 21:03:34 -0600

    Initial commit

---
+++ src/main/java/com/braldo/newjavaversions/NewJavaVersionsApplication.java
@@ -0,0 +1,13 @@
+package com.braldo.newjavaversions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJavaVersionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewJavaVersionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/braldo/newjavaversions/NewJavaVersionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braldo.newjavaversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJavaVersionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
